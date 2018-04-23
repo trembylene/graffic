38ca6ca1bd83cc6a08188d4ced906e8c57f64774
Daniel Kaga <daniel.kaga@gmail.com>
2018-04-23 16:02:32 +1000

    added pundit gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pundit
 
-   def configure_permitted_parameters
+  after_action :verify_authorized, except: [:index, :search], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :search], unless: :skip_pundit?
+
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:alias, :type])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:alias, :avatar, :info, :type])
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 end
