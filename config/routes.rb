bbeeb100a5970d50554fc75bfacc89563796bda9
Daniel Kaga <daniel.kaga@gmail.com>
2018-04-23 11:59:12 +1000

    Changed /art url to alias 'street_art'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:show]
-  resources :arts, except: [:destroy] do
+  resources :arts, except: [:destroy], :path => "street_art"do
     collection do
       get 'search'
     end
