2949627a1058ed5f5714a646f805eb12ac685f3f
Sergio Murakami <sergio_murakami@hotmail.com>
2018-04-27 13:40:33 +1000

    Created the link to artist profile. Created funcionality at the numbers statistic at profile - count is working. User Show Page just render one card instead many(my street art, uploaded art and hearted art).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show, :my_street_art]
 
   def show
-    @arts = policy_scope(Art).order(created_at: :desc)
     @user = User.find(params[:id])
     authorize @user
+    @my_arts = policy_scope(Art).where(artist: @user)
+    @hearted_arts = @user.votes.up.votables
+    @uploaded_arts = policy_scope(Art).where(user: @user)
   end
 end
