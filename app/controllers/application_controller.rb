207e82303e232a394dcb4d6521a6520e7259cd14
Daniel Kaga <daniel.kaga@gmail.com>
2018-04-30 15:05:13 +1000

    fixed footer to bottom of pages and set redirect to current_path after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,26 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:alias, :avatar, :info, :type])
   end
 
+  before_action :store_user_location!, if: :storable_location?
+
   private
+   # Its important that the location is NOT stored if:
+   # - The request method is not GET (non idempotent)
+   # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an
+   #    infinite redirect loop.
+   # - The request is an Ajax request as this can lead to very unexpected behaviour.
+  def storable_location?
+   request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+   # :user is the scope we are authenticating
+   store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+  stored_location_for(resource_or_scope) || super
+  end
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
